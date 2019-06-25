988a2daf3d14a0c37b4cad4f3f220bb0a2422457
Mariusz Chrobak <mchrobak@software.altkom.pl>
2019-06-25 15:40:03 +0200

    add test action

--- InsuranceSalesSystem/PricingService.Web/Controllers/PricingController.cs
+++ InsuranceSalesSystem/PricingService.Web/Controllers/PricingController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using PricingService.Api.Dto;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PricingService.Web.Controllers
@@ -21,6 +22,23 @@ namespace PricingService.Web.Controllers
         {
             return await Task.FromResult("Test success");
         }
+
+        [HttpGet("TestDb")]
+        public async Task<CalculatePriceResponseDto> TestDb()
+        {
+            var request = new CalculatePriceRequestDto()
+            {
+                PolicyHolderAge = 25,
+                PolicyStartDate = new System.DateTime(2018, 12, 1),
+                ProductCode = "GOLDEN_HEALTH",
+                SelectedCovers = new List<string>() { "COVER1", "COVER2" }
+            };
+
+            CalculatePriceResponseDto response = await mediator.Send(request);
+
+            return response;
+        }
+
 
         [HttpPost]
         public async Task<CalculatePriceResponseDto> CalculatePrice([FromBody] CalculatePriceRequestDto request)
