9fc4eb98cc1189410db814e7c77dec268b6bd6f1
Mariusz Chrobak <mchrobak@software.altkom.pl>
2019-11-20 09:27:47 +0100

    added Serilog to PolicyService project

--- InsuranceSalesSystem/PolicyService.Web/Startup.cs
+++ InsuranceSalesSystem/PolicyService.Web/Startup.cs
@@ -4,20 +4,48 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using PolicyService.Bo.Handlers;
 using PolicyService.Bo.Infrastructure.Communication.Events;
 using PolicyService.Bo.Infrastructure.Communication.REST;
 using PolicyService.Bo.Infrastructure.Database;
+using PolicyService.Web.Infrastructure;
 using PricingService.Web.Infrastracture;
 using RabbitMQ.Client;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
+using System;
 
 namespace PolicyService.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
         {
             Configuration = configuration;
+
+            //TODO: check if configuring serilog must be in ctor and if can use Configuration prop instead building configuration here
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", reloadOnChange: true, optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            var elasticUri = Configuration["ElasticConfiguration:Uri"];
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Debug()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri))
+                {
+                    AutoRegisterTemplate = true,
+                    MinimumLogEventLevel = LogEventLevel.Verbose,
+                    FailureCallback = e => Console.WriteLine("ES ex: " + e.MessageTemplate)
+                })
+            .CreateLogger();
         }
 
         public IConfiguration Configuration { get; }
@@ -30,7 +58,7 @@ namespace PolicyService.Web
             services.AddDbContext<PolicyDbContext>(options =>
              options.UseSqlServer(Configuration.GetConnectionString("PolicyDb")));
             services.AddScoped(x => new PricingApiFacade(Configuration.GetSection("ApiUrls")["PricingApiUrl"]));
-            services.AddScoped<IConnectionFactory>(x => 
+            services.AddScoped<IConnectionFactory>(x =>
                 new ConnectionFactory()
                 {
                     HostName = Configuration.GetSection("RabbitMq")["HostName"]
@@ -39,13 +67,20 @@ namespace PolicyService.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            //SelfLog will log problems with connection with ES to container logs
+            //TODO check why Elasticsearch.Net.ElasticsearchClientException: Connection refused. is logged even if it is working - maybe container with api starts faster than with ES container
+            Serilog.Debugging.SelfLog.Enable(message => Console.WriteLine(message));
+
+            loggerFactory.AddSerilog();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.ConfigureExceptionHandler();
             app.UseMvc();
             app.InitializeDatabase();
         }
